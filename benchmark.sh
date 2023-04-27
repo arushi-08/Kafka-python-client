ec84fddfa91ace6f0a9d6cf9dfe1ba2e44e8b76f
AlphaRoy14 <AlphaRoy14@gmail.com>
2023-04-27 15:52:51 -0700

    dockerize benchmarking

---
+++ benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the producer_benchmark.py script and print its output to the screen
+python producer_benchmark.py | tee producer_output.txt &
+
+# Wait for a few seconds to let the producer start sending messages
+sleep 5
+
+# Run the consumer_benchmark.py script and print its output to the screen
+python consumer_benchmark.py | tee consumer_output.txt
